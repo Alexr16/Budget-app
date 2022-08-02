a876a9ac8c9a17a8cd7584d1f971d1408aeb8dc4
Alexr16 <j.alex-1996@hotmail.com>
2022-08-02 16:18:55 -0500

    Add test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
     has_many :expenses, dependent: :destroy, foreign_key: 'author_id'
 
     validates :name, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
 end
