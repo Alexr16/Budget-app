ea901909a9ae1f59c23f59ba8744d673c1eff279
Alexr16 <j.alex-1996@hotmail.com>
2022-08-02 16:19:21 -0500

    Add test for expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -5,4 +5,6 @@ class Expense < ApplicationRecord
 
     validates :name, presence: true
     validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :author_id, presence: true
+    validates :group_id, presence: true
 end
